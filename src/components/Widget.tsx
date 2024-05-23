df341d846fb3101f6d13a2572ef44630b4854f9e
José Ricardo <josericardosaraiva7@gmail.com>
2024-05-23 17:30:18 -0300

    feat: widgetform & direcionamento ao feedbacktype

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,16 +1,23 @@
 import { ChatTeardropDots } from "@phosphor-icons/react"
+import { Popover, PopoverButton, PopoverPanel } from '@headlessui/react'
+import { WidgetForm } from "./WidgetForm";
 
 export function Widget() {
+    
     return (
-        <div className="absolute bottom-5 right-5">
-            <button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+        <Popover className="absolute bottom-5 right-5 md:bottom-8 md:right-8 flex flex-col items-end">
+            <PopoverPanel>
+                <WidgetForm />
+            </PopoverPanel>
+            
+            <PopoverButton className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group focus:outline-none">
                 <ChatTeardropDots className="w-6 h-6"/>
 
                 <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
                     <span className="pl-2"></span>
                     Feedback
                 </span>
-            </button>
-        </div>
+            </PopoverButton>
+        </Popover>
     );
 }
